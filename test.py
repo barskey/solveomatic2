1b9bc39bda108fa76f4a2c032107cbb4f2602532
Steve Barskey <>
2020-04-01 15:07:39 -0700

    sync

--- test.py
+++ test.py
@@ -6,16 +6,28 @@ import cv2
 from vision2 import grab_colors
 from lookups import PATTERNS
 
+# ----- Window setup -----
 sg.theme('Dark Grey')
 #sg.set_options(element_padding=(0, 0))
-
 title = sg.Text('Solve-O-Matic!')
 
-solveto_img = sg.Image('images/_solid.png', size=(60, 60), key='__SOLVETOIMG__')
+# ----- Intro Screen window layout -----
+layout_intro = [[
+    sg.Column([
+        [sg.Text('Solve-O-Matic')],
+        [sg.Text('Insert Cube')],
+        [sg.Text('to get started...')],
+        [sg.Text('')],
+        [sg.Button('Go', size=(11,2), key='__GO__')]
+    ], element_justification='center')
+]]
+
+# ----- User Input window layout -----
+solveto_img = sg.Image('images/_solid.png', size=(50, 50), key='__SOLVETOIMG__')
 col_left = sg.Column([
     [sg.Sizer(200, 10)],
     [sg.Text('1.'), sg.Button('Scan Cube', size=(11, 1))],
-    [sg.Text('2.'), sg.Combo(list(PATTERNS.keys()), default_value='Solid Cube', size=(10,2),key='__SOLVETO__')],
+    [sg.Text('2.'), sg.Combo(list(PATTERNS.keys()), default_value='Solid Cube', key='__SOLVETO__')],
     [sg.Text('', size=(3, 1)), solveto_img],
     [sg.Text('3.'), sg.Button('Solve!', size=(11, 1), disabled=True)]
 ])
@@ -30,14 +42,26 @@ col_right = sg.Column([
 
 layout_input = [[col_left, col_right]]
 
-# create the window and show it without the plot
-window = sg.Window('Solve-O-Matic', layout_input, size=(480, 320), no_titlebar=True, keep_on_top=True, finalize=True)
+# ----- Calibration window layout -----
+# layout_calibrate = 
+
+# create the window
+window = sg.Window('Solve-O-Matic', layout_intro, size=(480, 320), no_titlebar=True, keep_on_top=True, finalize=True)
+
+state = 0 # intro
+layouts = [layout_intro, layout_input]
+
+def change_state(newstate):
+    state = newstate
+    window.layout = layouts[state]
 
 # ---===--- Event LOOP Read and display frames, operate the GUI --- #
 while True:
     event, values = window.Read(timeout=20, timeout_key='timeout')
     if event in ('Quit', None):
         break
+    elif event == '__GO__':
+        change_state(1)
 
     i = 'images/{}'.format(PATTERNS[values['__SOLVETO__']][0])
     solveto_img.Update(filename=i)
