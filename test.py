1fc7a7e9b58cad61c6bb55b19d1e5ede718a135b
Steve Barskey <>
2020-04-02 15:17:01 -0700

    sync

--- test.py
+++ test.py
@@ -9,94 +9,49 @@ from lookups import PATTERNS
 sg.theme('Dark Grey')
 #sg.set_options(element_padding=(0, 0))
 title = sg.Text('Solve-O-Matic!')
-# ----- Intro Screen window layout | win_intro -----
-layout_intro = [[
-    sg.Column([
-        [sg.Sizer(440, 10)],
-        [sg.Text('Solve-O-Matic')],
-        [sg.Text('Insert Cube')],
-        [sg.Text('to get started...')],
-        [sg.Text('')],
-        [sg.Button('Go', size=(11,2))]
-    ], element_justification='center')
-]]
-win_intro = sg.Window('Solve-O-Matic', layout_intro, size=(480, 320), no_titlebar=True, return_keyboard_events=True)
-win_intro_active = True
 
-# ----- User Input window layout | win_input -----
-def win_input_layout():
-    return ([[
-        sg.Column([
-            [sg.Sizer(200, 10)],  # pads col to 200 pix
-            [sg.Text('1.'), sg.Button('Scan Cube', size=(11, 1))],
-            [sg.Text('2.'), sg.Combo(list(PATTERNS.keys()), default_value='Solid Cube', key='-SOLVETO-')],
-            [sg.Text('', size=(3, 1)), sg.Image('images/_solid.png', key='-SOLVETOIMG-')],
-            [sg.Text('3.'), sg.Button('Solve!', size=(11, 1), disabled=True)]
-        ]),
-        sg.Column([
-            [sg.Sizer(200, 10)],  # pads col to 200 pix
-            [sg.Graph(canvas_size=(160, 160), graph_bottom_left=(0, 0), graph_top_right=(160, 160), key='-GRAPH-')], #  canvas to display image
-            [sg.Quit(), sg.Button('Calibrate')]
-        ], element_justification='center')
-    ]])
-win_input = sg.Window('Solve-O-Matic', win_input_layout(), size=(480, 320), no_titlebar=True)
-win_input_active = False
+# ----- User Input window layout -----
+layout = [[
+    sg.Column([
+        [sg.Sizer(200, 10)],  # pads col to 200 pix
+        [sg.Button('GRIP', size=(11,1))],
+        [sg.Button('SCAN', size=(11, 1)), disabled=True],
+        [sg.Combo(list(PATTERNS.keys()), default_value='Solid Cube', key='-SOLVETO-')],
+        [sg.Image('images/_solid.png', key='-SOLVETOIMG-')],
+        [sg.Button('SOLVE!', size=(11, 1), disabled=True)]
+    ]),
+    sg.Column([
+        [sg.Sizer(200, 10)],  # pads col to 200 pix
+        [sg.Graph(canvas_size=(160, 160), graph_bottom_left=(0, 0), graph_top_right=(160, 160), key='-GRAPH-')], #  canvas to display image
+        [sg.Quit(), sg.Button('Calibrate')]
+    ], element_justification='center'),
+    sg.Text('Insert cube and GRIP to continue...', key='-INFO-')
+]])
+window = sg.Window('Solve-O-Matic', layout, size=(480, 320), no_titlebar=True, return_key_events=True)
 
 # ----- Calibration window layout | win_cal -----
-def win_cal_layout():
+def cal_layout():
     return ([[
         sg.Quit()
     ]])
-win_cal = sg.Window('Solve-O-Matic', win_cal_layout(), size=(480, 320), no_titlebar=True)
-win_cal_active = False
-
-win_intro.BringToFront()
 
 # ---===--- Event LOOP Read and display frames, operate the GUI --- #
 while True:
-    if win_intro_active:
-        button1, values1 = win_intro.read(timeout=50, timeout_key='timeout')
-    
-        if button1 in (None, 'Escape:9'):
+    button, values = window.read(timeout=50)
+    if button in (None, 'Quit'):
+        break
+    elif button == 'Calibrate':
+       window_cal = sg.Window('Solve-O-Matic', cal_layout(), size=(480, 320), no_titlebar=True)
+       while True:
+        calbtn, calvals = window_cal.read(timeout=50)
+        if calbtn in (None, 'Quit'):
+            window_cal.Close()
             break
 
-        if button1 == 'Go' and not win_input_active:
-            print(button1)
-            win_input_active = True
-            win_intro_active = False
-            win_input.un_hide()
-            win_intro.hide()
+    i = 'images/{}'.format(PATTERNS[values2['-SOLVETO-']][0])
+    window['-SOLVETOIMG-'].Update(filename=i)
+    frame = grab_colors()
+    img_bytes = cv2.imencode('.png', frame)[1].tobytes()     # Convert the image to PNG Bytes
+    window['-GRAPH-'].draw_image(location=(0, 160), data=img_bytes)
 
-    elif win_input_active:
-        button2, values2 = win_input.read(timeout=50)
-        if button2 in (None, 'Quit'):
-            print(button2)
-            win_intro_active = True
-            win_input_active = False
-            win_intro.un_hide()
-            win_input.hide()
-        elif button2 == 'Calibrate':
-            print(button2)
-            win_cal_active = True
-            win_input_active = False
-            win_cal.un_hide()
-            win_input_active.hide()
-
-        i = 'images/{}'.format(PATTERNS[values2['-SOLVETO-']][0])
-        win_input['-SOLVETOIMG-'].Update(filename=i)
-        frame = grab_colors()
-        img_bytes = cv2.imencode('.png', frame)[1].tobytes()     # Convert the image to PNG Bytes
-        win_input['-GRAPH-'].draw_image(location=(0, 160), data=img_bytes)
-
-    elif win_cal_active:
-        button3, values3 = win_cal.read(timeout=50)
-        if button3 in (None, 'Quit'):
-            print(button3)
-            win_input_active = True
-            win_cal_active = False
-            win_input.un_hide()
-            win_cal.hide()
-
-win_intro.Close()
-win_input.Close()
-win_cal.Close()
+window.Close()
