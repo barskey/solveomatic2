1f4018b8d3ef30bc6a05401f807c1c7eaa82661d
Steve Barskey <>
2020-04-03 14:51:01 -0700

    sync

--- test.py
+++ test.py
@@ -15,23 +15,23 @@ col1 = sg.Column([
         [sg.Frame('Step 1', [
             [sg.Button('GRIP', size=(11,1))],
             [sg.Button('SCAN', size=(11, 1), disabled=True)]
-        ], size=(480, 240), pad=(0, 0))],
+        ], pad=(0, 0))],
         [sg.Frame('Step 2', [
             [sg.Combo(list(PATTERNS.keys()), default_value='Solid Cube', key='-SOLVETO-')],
             [sg.Image('images/_solid.png', key='-SOLVETOIMG-')]
-        ], size=(480, 240), pad=(0, 0))],
+        ], pad=(0, 0))],
         [sg.Frame('Step 3', [
             [sg.Button('SOLVE!', size=(11, 1), disabled=True)]
-        ], size=(480, 240), pad=(0, 0))]
-    ], element_justification='center', pad=(0, 0))
+        ], pad=(0, 0))]
+    ], element_justification='center', size=(240, 240), pad=(0, 0))
 col2 = sg.Column([
         [sg.Sizer(200, 1)],  # pads col to 200 pix
         [sg.Graph(canvas_size=(160, 160), graph_bottom_left=(0, 0), graph_top_right=(160, 160), key='-GRAPH-')], #  canvas to display image
         [sg.Quit(), sg.Button('Calibrate')]
-    ], element_justification='center')
+    ], element_justification='center', size=(240, 240))
 col3 = sg.Column([
     [sg.Text('Insert cube and GRIP to continue...', font=('Computerfont', 14, ''), key='-INFO-')]
-])
+], element_justification='center', size=(240, 240))
 layout = [[col1, col2], [col3]]
 window = sg.Window('Solve-O-Matic', layout, size=(480, 320), no_titlebar=True, return_keyboard_events=True)
 
