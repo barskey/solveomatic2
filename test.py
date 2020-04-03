26ecfec68a6e404b6fa9ac1fe75397bee75aebb4
Steve Barskey <>
2020-04-03 15:28:40 -0700

    sync

--- test.py
+++ test.py
@@ -12,26 +12,28 @@ title = sg.Text('Solve-O-Matic!')
 
 # ----- User Input window layout ----- #
 col1 = sg.Column([
-        [sg.Frame('Step 1', [
+        [sg.Frame('Step 1 | Insert cube',
+            [sg.Sizer(220, 1)],
             [sg.Button('GRIP', size=(11,1))],
             [sg.Button('SCAN', size=(11, 1), disabled=True)]
         ], pad=(0, 0))],
-        [sg.Frame('Step 2', [
+        [sg.Frame('Step 2 | Pick a pattern:', [
+            [sg.Sizer(220, 1)],
             [sg.Combo(list(PATTERNS.keys()), default_value='Solid Cube', key='-SOLVETO-')],
             [sg.Image('images/_solid.png', key='-SOLVETOIMG-')]
         ], pad=(0, 0))],
-        [sg.Frame('Step 3', [
+        [sg.Frame('Step 3 | Go', [
             [sg.Button('SOLVE!', size=(11, 1), disabled=True)]
         ], pad=(0, 0))]
     ], element_justification='center', size=(240, 240), pad=(0, 0))
 col2 = sg.Column([
-        [sg.Sizer(200, 1)],  # pads col to 200 pix
+        [sg.Sizer(220, 1)],  # pads col to 220 pix
         [sg.Graph(canvas_size=(160, 160), graph_bottom_left=(0, 0), graph_top_right=(160, 160), key='-GRAPH-')], #  canvas to display image
         [sg.Quit(), sg.Button('Calibrate')]
     ], element_justification='center', size=(240, 240))
 col3 = sg.Column([
     [sg.Text('Insert cube and GRIP to continue...', font=('Computerfont', 14, ''), key='-INFO-')]
-], element_justification='center', size=(240, 240))
+], element_justification='center')
 layout = [[col1, col2], [col3]]
 window = sg.Window('Solve-O-Matic', layout, size=(480, 320), no_titlebar=True, return_keyboard_events=True)
 
