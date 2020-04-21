23f873c359b8bfd6210221793a043ab40b6126ea
Steve Barskey <>
2020-04-21 13:41:55 -0700

    sync

--- test.py
+++ test.py
@@ -57,7 +57,7 @@ for p, l in PATTERNS.items():
                               border_width=0)]
     count += 1
 if solveto_row:
-    solveto_layout += solveto_row
+    solveto_layout += [solveto_row]
 solveto_window = sg.Window('Solve To', solveto_layout, size=(400, 300), no_titlebar=True)
 solveto_window.read()
 solveto_window.close()
