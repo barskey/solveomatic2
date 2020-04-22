591098b848a5df17f2923ebb767f6ed0253b32c4
Steve Barskey <>
2020-04-22 13:46:10 -0700

    sync

--- test.py
+++ test.py
@@ -113,7 +113,6 @@ while True:
         window_cal = sg.Window('Solve-O-Matic', cal_layout(), size=(480, 320), no_titlebar=True)
         while True:
             calbtn, calvals = window_cal.read(timeout=50)
-            print(calbtn)
             if calbtn in (None, 'Quit'):
                 window_cal.Close()
                 break
