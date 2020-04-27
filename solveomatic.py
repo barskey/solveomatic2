ae2ec6fd0d762b3a6d8ffc6befe6a8defc335159
Steve Barskey <>
2020-04-27 15:15:11 -0700

    sync

--- solveomatic.py
+++ solveomatic.py
@@ -1,6 +1,6 @@
 from threading import Thread
 from vision2 import grab_colors
-img_bytes = b''
+import vision_params
 
 class imgThread(Thread):
     def __init__(self):
@@ -9,7 +9,7 @@ class imgThread(Thread):
         self.start()
     def run(self):
         while True:
-            img_bytes = grab_colors()
+            vision_params.img_bytes = grab_colors()
 
 
 imgThread()
